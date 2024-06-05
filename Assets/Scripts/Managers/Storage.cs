2de66794417931da54fd180fb115531bad6872ad
M1st3r-G <g.haegerbaeumer@gmx.de>
2024-06-05 12:31:43 +0200

    Added Storage Controller

---
+++ Assets/Scripts/Managers/Storage.cs
@@ -0,0 +1,31 @@
+﻿using Data;
+using UnityEngine;
+
+namespace Managers
+{
+    public class Storage : MonoBehaviour
+    {
+        private FlowerInstance[] _allPots;
+        private FlowerInstance[] _allSeeds;
+        
+        public static Storage Instance { get; private set; }
+        
+        private void Awake()
+        {
+            _allPots = new FlowerInstance[12];
+            _allSeeds = new FlowerInstance[6];
+            Instance = this;
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this) Instance = this;
+        }
+
+        public FlowerInstance[] GetSeeds() => _allSeeds;
+        public void StoreSeeds(FlowerInstance[] seeds) => _allSeeds = seeds;
+
+        public void SetPlant(int index, FlowerInstance flower) => _allPots[index] = flower;
+        public FlowerInstance GetFlower(int index) => _allPots[index];
+    }
+}
