e3f8c974ed3b40af019c26ef2f2a396973b4d636
M1st3r-G <g.haegerbaeumer@gmx.de>
2024-06-06 22:55:05 +0200

    CleanUp of FlowerInstance
    Also Added OnChange Event

--- Assets/Scripts/Data/FlowerInstance.cs
+++ Assets/Scripts/Data/FlowerInstance.cs
@@ -1,36 +1,40 @@
-﻿using System.Linq.Expressions;
-using Managers;
+﻿using Managers;
 using UnityEngine;
 
 namespace Data
 {
     public class FlowerInstance
     {
+        #region SetUpAndFields
+
         private enum GrowthState
         {
             Seed, Sprout, Flower, Dead
         }
-
-        public bool IsReplantable => _state == GrowthState.Sprout;
+        
+        // State
         
         private readonly FlowerData _type;  // The Type of Flower
         private GrowthState _state;         // The GrowthState
         private int _lastWater;             // Time Since Watering
         private int _growthCounter;         // How Many Days Past since start of Phase
-
-        private int Rating
-        {
-            get => _rating;
-            set
-            {
-                _rating = value;
-                if (_rating <= 50) _state = GrowthState.Dead;
-            }
-        }
         private int _rating;
-
         private bool _isReplant;
         
+        // Public
+        public bool IsReplantable => _state == GrowthState.Sprout;
+
+        public int CalculateStars() => _rating switch
+        {
+            >= 90 => 3, 
+            >= 75 => 2, 
+            >= 50 => 1, 
+            _ => 0
+        };
+        
+        public delegate void OnChangeEvent();
+        public OnChangeEvent OnChange;
+
         public FlowerInstance(FlowerData type, Environment potEnv)
         {
             _type = type;
@@ -43,37 +47,19 @@ namespace Data
             TimeManager.OnTimeIncrease += Grow;
         }
 
-        public int CalculateStars() => _rating switch
-        {
-            >= 90 => 3, 
-            >= 75 => 2, 
-            >= 50 => 1, 
-            _ => 0
-        };
+        #endregion
         
-        private static int CalculatePenalty(FlowerData flower, Environment potEnv)
-        {
-            int counter = 0;
-            if (flower.PreferredEnvironment.lichtkeimer != potEnv.lichtkeimer) counter += 10;
-            if (flower.HatedSoil == potEnv.soil) counter += 30;
-            else if (flower.PreferredEnvironment.soil != potEnv.soil) counter += 10;
-            return counter;
-        }
+        #region StateChange
 
-        /// <summary>
-        /// Only if Plant is Sprout
-        /// </summary>
-        /// <param name="fertilizer">The Fertilizer in the New Environment</param>
         public void Replant(Environment.FertilizerType fertilizer)
         {
             Debug.Log($"{_type.FlowerName} Got replant with {fertilizer} as Fertilizer");
             _isReplant = true;
-            if (fertilizer != _type.Fertilizer) Rating -= 10;
+            if (fertilizer != _type.Fertilizer) DecreaseRating(10);
+            
+            OnChange?.Invoke();
         }
         
-        /// <summary>
-        /// As Long as the Plant object exist, this is called at the Start of each day
-        /// </summary>
         private void Grow()
         {
             // Increase internal Timers
@@ -81,7 +67,7 @@ namespace Data
             _lastWater++;
             
             // If too dry, The Plant takes Damage
-            if (_lastWater > _type.WaterFrequency) Rating -= (_lastWater - _type.WaterFrequency) * 5;
+            if (_lastWater > _type.WaterFrequency) DecreaseRating((_lastWater - _type.WaterFrequency) * 5);
             
             // Grows
             switch (_state)
@@ -98,7 +84,7 @@ namespace Data
                     {
                         if (!_isReplant)
                         {
-                            Rating -= (_growthCounter - _type.TimeToBloom) * 5;
+                            DecreaseRating((_growthCounter - _type.TimeToBloom) * 5);
                         }
                         else
                         {
@@ -115,21 +101,43 @@ namespace Data
                     TimeManager.OnTimeIncrease -= Grow;
                     break;
             }
+            
+            OnChange?.Invoke();
         }
 
-        /// <summary>
-        /// Waters the Plant
-        /// </summary>
         public void Water()
         {
             // To Wet -> Takes Damage
-            if (_lastWater < _type.WaterFrequency)  Rating -= (_type.WaterFrequency - _lastWater) * 5;
+            if (_lastWater < _type.WaterFrequency)  DecreaseRating((_type.WaterFrequency - _lastWater) * 5);
             _lastWater = 0;
+            
+            OnChange?.Invoke();
         }
 
+        #endregion
+
+        #region Utility
+
+        private void DecreaseRating(int amount)
+        {
+            _rating -= amount;
+            if (_rating <= 50) _state = GrowthState.Dead;
+        }
+        
+        private static int CalculatePenalty(FlowerData flower, Environment potEnv)
+        {
+            int counter = 0;
+            if (flower.PreferredEnvironment.lichtkeimer != potEnv.lichtkeimer) counter += 10;
+            if (flower.HatedSoil == potEnv.soil) counter += 30;
+            else if (flower.PreferredEnvironment.soil != potEnv.soil) counter += 10;
+            return counter;
+        }
+        
         public override string ToString()
         {
             return $"Diese {_type.FlowerName} ist ein(e) {_state}\nRanking: {_rating}\nLW:{_lastWater};GC:{_growthCounter}";
         }
+
+        #endregion
     }
 }
